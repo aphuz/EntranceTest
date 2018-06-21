4160ac606415d6953dd405438ffb4c15f83fae7b
PAPhuc <paphuc@tma.com.vn>
2018-06-21 11:35:16 +0700

    Update description for apis

--- src/app/shared/mocks/api.mock.ts
+++ src/app/shared/mocks/api.mock.ts
@@ -1,16 +1,16 @@
 import { Api } from '../models/api.model';
 
 export const APIs: Api[] = [
-  { id: 1, name: 'getAllAnswerURL', url: '/getallanswers', description: "Get all user" },
-  { id: 2, name: 'loginURL', url: '/login', description: "Get all user" },
-  { id: 3, name: 'getAllCategoryURL', url: '/getallcategories', description: "Get all user" },
-  { id: 4, name: 'getQuestionPDFURL', url: '/exportRandomExamination', description: "Get all user" },
-  { id: 5, name: 'exportExamByInterviewCode', url: '/exportExamByInterviewCode', description: "Get all user" },
-  { id: 6, name: 'getAnswerPDFURL', url: '/exportanswerPDF/', description: "Get all user" },
-  { id: 7, name: 'uploadFileURL', url: '/uploadfile', description: "Get all user" },
-  { id: 8, name: 'getAllInterviewURL', url: '/getallinterviews', description: "Get all user" },
-  { id: 9, name: 'getAllKindURL', url: '/getallkinds', description: "Get all user" },
-  { id: 10, name: 'getQuestionsByProgrammingLanguageURL', url: '/getQuestionsByProgrammingLanguage', description: "Get all user" },
-  { id: 11, name: 'getAllQuestionURL', url: '/getallquestions', description: "Get all user" },
+  { id: 1, name: 'getAllAnswerURL', url: '/getallanswers', description: "Get all answers" },
+  { id: 2, name: 'loginURL', url: '/login', description: "Login" },
+  { id: 3, name: 'getAllCategoryURL', url: '/getallcategories', description: "Get all categories" },
+  { id: 4, name: 'getQuestionPDFURL', url: '/exportRandomExamination', description: "Get PDF file" },
+  { id: 5, name: 'exportExamByInterviewCode', url: '/exportExamByInterviewCode', description: "Get PDF file" },
+  { id: 6, name: 'getAnswerPDFURL', url: '/exportanswerPDF/', description: "Get PDF file" },
+  { id: 7, name: 'uploadFileURL', url: '/uploadfile', description: "Upload file" },
+  { id: 8, name: 'getAllInterviewURL', url: '/getallinterviews', description: "Get all interviews" },
+  { id: 9, name: 'getAllKindURL', url: '/getallkinds', description: "Get all kinds" },
+  { id: 10, name: 'getQuestionsByProgrammingLanguageURL', url: '/getQuestionsByProgrammingLanguage', description: "Get questions" },
+  { id: 11, name: 'getAllQuestionURL', url: '/getallquestions', description: "Get all questions" },
   { id: 12, name: 'getAllUserURL', url: '/getallusers', description: "Get all user" }
   ];
